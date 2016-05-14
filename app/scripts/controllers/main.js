9baab78e461fd3645d945930af8f93a333408f78
Kevin Caraos <Kevincaraos@Kevins-MacBook-Air.local>
2016-05-14 14:39:37 +1000

    Init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name websiteWorkApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the websiteWorkApp
+ */
+angular.module('websiteWorkApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
